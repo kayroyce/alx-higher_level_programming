de1a42052797cac7d9a274d5fda513bd58d1c142
kayroyce <kayroyce2005@gmail.com>
2022-06-27 16:12:09 -0700

    commit

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,1 +1,3 @@
-
+#!/usr/bin/python3
+number = 5.14359
+print("Float: {:.2f}".format(number))
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,1 +1,4 @@
-
+#!/usr/bin/python3
+str = "Holberton School"
+print("{}".format(str) * 3)
+print(str[:9])
