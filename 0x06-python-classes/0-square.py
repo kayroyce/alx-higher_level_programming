895562554012a96ed61dc0ab115cce848937462d
kayroyce <kayroyce2005@gmail.com>
2022-07-19 08:18:20 -0700

    taskk commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    def __int__(self):
+        pass
+
+if __name__ == "__main":
+    Square()
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+
+class Square:
+
+    def __int__(self, size):
+
+        self.__size = size
+
+if __name__ == "__main__":
+    Square()
