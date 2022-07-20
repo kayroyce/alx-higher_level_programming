5a6cbf2886bd35c2e4a79613b30f20d6839113aa
kayroyce <kayroyce2005@gmail.com>
2022-07-20 08:45:38 -0700

    xtask

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,5 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 132b12dece8a5f5e556cdb3f430c066fd12d5de2
 class Square:
     """Class defining a square"""
     def __int__(self, size):
