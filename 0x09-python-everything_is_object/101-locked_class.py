fc093bbd18592fb85e164d4ab9345f07dc83cf5b
kayroyce <kayroyce2005@gmail.com>
2022-07-27 04:18:52 -0700

    correction

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,9 +7,9 @@ https://www.python-course.eu/python3_slots.php
 
 
 class LockedClass():
-     """
-     prevent user from creating new instance attribute dynamically
-     unless attribute is "first_name"
-     """
+    """
+    prevent user from creating new instance attribute dynamically
+    unless attribute is "first_name"
+    """
 
-__slots__ = "first_name"
+    __slots__ = "first_name"
