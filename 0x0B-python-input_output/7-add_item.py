dc9a3d03fc3272f2bbbc066bd2747f03a138874e
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-08-03 00:56:02 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     import sys
     import json
     save_to_json_file = \
-        __import__('7-save_to_json_file').save_to_json_file
+        __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     filename = "add_item.json"
     with open(filename, 'a+') as f:  # Create add_item.json, if necessary
