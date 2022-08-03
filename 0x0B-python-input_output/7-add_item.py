1bd9b3c11b3ae90af38dafbfcdf88147d057e71f
kayroyce <102330728+kayroyce@users.noreply.github.com>
2022-08-03 01:35:17 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""
-Contains function that adds and saves to Python obj to JSON file; loads objects
-"""
+'''7-add_item.py
+    a script that adds all arguments to a Python list,
+    and then save them to a file
+'''
+
+from os import path
+import sys
 
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    argv = sys.argv[1:]
 
-filename = "add_item.json"
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
-
-save_to_json_file(existing_content + argv[1:], filename)
+    file_path = "add_item.json"
+    if path.exists(file_path):
+        list_obj = load_from_json_file(file_path)
+    else:
+        list_obj = list()
+    
+    save_to_json_file(list_obj + argv, file_path)
+    print(end="")
