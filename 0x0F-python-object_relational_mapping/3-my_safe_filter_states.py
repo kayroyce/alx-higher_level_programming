47550bd22d2af7493894efce4624df7d8a3a9fba
kayroyce <kayroyce2005@gmail.com>
2022-10-11 07:13:52 -0700

    task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     
     with db.cursor() as cur:
-        cursor.execute("SELECT * FROM states \
+        cursor.execute(("SELECT * FROM states \
                 WHERE name LIKE BINARY %(name)s \
                 ORDER BY states.id ASC")
                 {
                     'name' = argv[4]
-                }
+                });
                  
         rows = cursor.fetchall()
 
