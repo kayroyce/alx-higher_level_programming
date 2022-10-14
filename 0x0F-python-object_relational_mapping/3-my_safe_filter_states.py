d31188dc3d8537a8afb693402f9369e8c6e89bf0
kayroyce <kayroyce2005@gmail.com>
2022-10-14 15:03:21 +0100

    console update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,30 +8,14 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     
     with db.cursor() as cur:
-<<<<<<< HEAD
-        cursor.execute(("SELECT * FROM states \
-                WHERE name LIKE BINARY %(name)s \
-                ORDER BY states.id ASC")
-                {
-                    'name' = argv[4]
-                });
-=======
         cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (searched_name, ))
->>>>>>> 3db0ac7e614545e2ff400d2b4973fa913161cb7d
                  
         rows = cursor.fetchall()
 
     if row is not None:
-<<<<<<< HEAD
-        for row in rows:
         print(row)
-=======
-        print(row)
-<<<<<<< HEAD
 
->>>>>>> 3db0ac7e614545e2ff400d2b4973fa913161cb7d
-=======
->>>>>>> 1820da226919c0aaeb6db8584fb9035744d6902d
