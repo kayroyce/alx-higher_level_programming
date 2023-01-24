6d0ee515379c7eba4dfe7344336b17abb7614ad7
Olawale Kolawole <102330728+kayroyce@users.noreply.github.com>
2023-01-24 13:32:43 +0100

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,8 @@
     reads and prints the content of a file
 */
 const fs = require('fs');
-const nameFile = process.argv[2];
 
-fs.readFile(nameFile, 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
